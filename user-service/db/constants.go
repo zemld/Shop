1c31e59e3d95286ebb4027e4f1c0df6a60b68e37
zemld <zemlyanukhind@yandex.ru>
2025-06-13 13:46:18 +0300

    Изменена видимость строк операций с бд

--- user-service/db/constants.go
+++ user-service/db/constants.go
@@ -1,9 +1,9 @@
 package db
 
 const (
-	usersDB               = "postgres://user:password@user-db:5432/userdb"
+	UsersDB               = "postgres://user:password@user-db:5432/userdb"
 	createUsersTableQuery = `CREATE TABLE IF NOT EXISTS users (
 		username TEXT PRIMARY KEY,
 		balance FLOAT NOT NULL DEFAULT 0.0`
-	checkUserRegisteredQuery = "SELECT 1 FROM users WHERE username = $1 LIMIT 1;"
+	CheckUserRegisteredQuery = "SELECT 1 FROM users WHERE username = $1 LIMIT 1;"
 )

d0f4f78fe1e6b3c74cf94eb5c2ed30572b3bc916
zemld <zemlyanukhind@yandex.ru>
2025-06-13 12:47:20 +0300

    Добавлены строки создания таблицы и проверки существования пользователя

--- user-service/db/constants.go
+++ user-service/db/constants.go
@@ -1,5 +1,9 @@
 package db
 
 const (
-	usersDB = "postgres://user:password@user-db:5432/userdb"
+	usersDB               = "postgres://user:password@user-db:5432/userdb"
+	createUsersTableQuery = `CREATE TABLE IF NOT EXISTS users (
+		username TEXT PRIMARY KEY,
+		balance FLOAT NOT NULL DEFAULT 0.0`
+	checkUserRegistered = "SELECT 1 FROM users WHERE username = $1 LIMIT 1;"
 )

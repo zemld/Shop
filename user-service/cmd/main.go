6bf4a2db23406d8accfda8083e2b720c8e397bb9
zemld <zemlyanukhind@yandex.ru>
2025-06-14 12:27:35 +0300

    Добавлена сваггер документация

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,13 +1,25 @@
-package cmd
+package main
 
 import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/zemld/Shop/user-service/handlers"
 )
 
+// @title User Service
+// @version 1.0
+// @description API for interacting with users.
+// @host localhost:8081
+// @BasePath /
 func main() {
 	router := chi.NewRouter()
 
+	router.Get("/v1/users/{userID}", handlers.CheckUserRegistered)
+
+	fs := http.FileServer(http.Dir("./docs"))
+	router.Handle("/docs/*", http.StripPrefix("/docs/", fs))
+	router.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8081/docs/swagger.json")))
 	http.ListenAndServe(":8081", router)
 }

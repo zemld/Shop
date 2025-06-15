be603686595143ed2bb39dbc5718f4d22a04a307
zemld <zemlyanukhind@yandex.ru>
2025-06-15 19:57:48 +0300

    Создана ручка для вставки товара и сваггер документация для нее

--- item-service/cmd/main.go
+++ item-service/cmd/main.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/zemld/Shop/item-service/handlers"
 )
 
+// @title Item Service
+// @version 1.0
+// @description API for interacting with items in storage.
+// @host localhost:8083
+// @BasePath /
 func main() {
 	router := chi.NewRouter()
 
+	router.Post("/v1/items/add", handlers.AddItemHandler)
+
+	fs := http.FileServer(http.Dir("./docs"))
+	router.Handle("/docs/*", http.StripPrefix("/docs/", fs))
+	router.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8083/docs/swagger.json")))
 	http.ListenAndServe(":8083", router)
 }

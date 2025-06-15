83c664f750b76ccf0b7e4c676282513973720123
zemld <zemlyanukhind@yandex.ru>
2025-06-15 23:06:51 +0300

    Добавлен комментарий с туду

--- item-service/domain/models/responses.go
+++ item-service/domain/models/responses.go
@@ -1,5 +1,7 @@
 package models
 
+// TODO: по-хорошему здесь нужен какой-то статус операции задавать.
+// Мб отдельный респонс для обновления количества товара.
 type ItemResponse struct {
 	Item    Item   `json:"item"`
 	Message string `json:"message"`

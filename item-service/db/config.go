7ba35c65fc607ea172e5d69f84e9b9d5f605be90
zemld <zemlyanukhind@yandex.ru>
2025-06-15 19:18:29 +0300

    Написаны функции для работы с бд

---
+++ item-service/db/config.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func getContext() (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	return ctx, cancel
+}
+
+func ConnectDB(dbConnection string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dbConnection)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := getContext()
+	defer cancel()
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func CreateTable(db *sql.DB, createTableQuery string) error {
+	if db == nil {
+		return nil
+	}
+	ctx, cancel := getContext()
+	defer cancel()
+	if _, err := db.ExecContext(ctx, createTableQuery); err != nil {
+		return err
+	}
+	return nil
+}
+
+func CloseDB(db *sql.DB) error {
+	if db == nil {
+		return nil
+	}
+	if err := db.Close(); err != nil {
+		return err
+	}
+	return nil
+}

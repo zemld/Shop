491a998a80a0add348ef61f2f105fef9eece8d83
zemld <zemlyanukhind@yandex.ru>
2025-06-15 14:21:23 +0300

    Написана ручка для регистрации админа

--- admin-service/cmd/main.go
+++ admin-service/cmd/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/zemld/Shop/admin-service/handlers"
 )
 
+// @title Gateway
+// @version 1.0
+// @description Admin service for managing administrators and items in the storage.
+// @host localhost:8082
+// @BasePath /
 func main() {
 	router := chi.NewRouter()
 
+	router.Post("/v1/admins/register", handlers.RegisterAdminHandler)
+
+	fs := http.FileServer(http.Dir("./docs"))
+	router.Handle("/docs/*", http.StripPrefix("/docs/", fs))
+	router.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8082/docs/swagger.json")))
 	http.ListenAndServe(":8082", router)
 }

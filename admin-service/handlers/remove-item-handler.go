c0b18814e5c217c57a97943c90b08e054bdb32f6
zemld <zemlyanukhind@yandex.ru>
2025-06-17 00:26:13 +0300

    Добавлена обработка ручки удаления товара

---
+++ admin-service/handlers/remove-item-handler.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/zemld/Shop/admin-service/domain/models"
+	"github.com/zemld/Shop/admin-service/internal"
+)
+
+// @description Remove an item.
+// @tag.name Items operations
+// @param secret query string true "Admin secret code for authorization"
+// @param name query string true "Item name which you want to remove"
+// @produce json
+// @success 200 {object} models.ItemResponse
+// @failure 400 {object} models.StatusResponse
+// @failure 403 {object} models.StatusResponse
+// @failure 500 {object} models.StatusResponse
+// @router /v1/items/remove [post]
+func RemoveItemHandler(w http.ResponseWriter, r *http.Request) {
+	isAdmin := internal.AuthAdmin(r.URL.Query().Get("secret"))
+	if !isAdmin {
+		internal.WriteResponse(w, models.StatusResponse{
+			Message: "You are not authorized to add items.",
+		}, http.StatusForbidden)
+		return
+	}
+
+	response, err := internal.SendRequestToItemService(internal.POST, r.URL.Path, r.URL.Query())
+	if err != nil {
+		internal.WriteResponse(w, models.StatusResponse{
+			Message: "Failed to remove item: " + err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	internal.TryParseResponseBodyAndWriteResponse(w, response, models.ItemResponseType)
+}

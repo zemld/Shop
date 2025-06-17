bc7fdca27babea442c5e759325ce8c971713b73c
zemld <zemlyanukhind@yandex.ru>
2025-06-17 21:44:45 +0300

    Добавлена модель для очереди

--- order-service/models/order.go
+++ order-service/models/order.go
@@ -9,3 +9,9 @@ type ItemInOrder struct {
 	Name   string `json:"name"`
 	Amount int    `json:"amount"`
 }
+
+type OrderMsg struct {
+	Items []ItemInOrder `json:"items"`
+	User  string        `json:"user"`
+	Id    int           `json:"id"`
+}

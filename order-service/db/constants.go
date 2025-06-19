41a277860a33e50c35aeb8da9a115017daa00a1c
zemld <zemlyanukhind@yandex.ru>
2025-06-19 23:08:51 +0300

    Исправлены запросы

--- order-service/db/constants.go
+++ order-service/db/constants.go
@@ -1,13 +1,13 @@
 package db
 
 const (
-	OutboxDB               = "postgres://order_outbox:password@order-outboxdb:5432/order-outbox-db"
+	OutboxDB               = "postgres://order-outbox:password@order-outbox-db:5432/order-outboxdb"
 	createOutboxTableQuery = `CREATE TABLE IF NOT EXISTS outbox (
-		id SERIAL NOT NULL,
-		created_at TIMESTAMP NOT NULL DEFAULT NOW() PRIMARY KEY,
-		order JSONB NOT NULL
-		);`
-	createOrderQuery         = "INSERT INTO outbox (order) VALUES ($1) RETURNING id;"
-	handleOrderQuery         = "DELETE FROM outbox WHERE id = $1;"
-	getUnhandeledOrdersQuery = `SELECT id, order FROM outbox WHERE created_at < NOW() ORDER BY created_at;`
+    id SERIAL NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW() PRIMARY KEY,
+    "order" JSONB NOT NULL
+);`
+	createOrderQuery         = `INSERT INTO outbox ("order") VALUES ($1) RETURNING id;`
+	handleOrderQuery         = `DELETE FROM outbox WHERE id = $1;`
+	getUnhandeledOrdersQuery = `SELECT id, "order" FROM outbox WHERE created_at < NOW() ORDER BY created_at;`
 )

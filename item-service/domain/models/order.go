7dfcb2b05c79954a1f66876cc3a66768f98e4848
zemld <zemlyanukhind@yandex.ru>
2025-06-19 09:45:52 +0300

    Добавлены модели для заказа

---
+++ item-service/domain/models/order.go
@@ -0,0 +1,17 @@
+package models
+
+type Order struct {
+	Items []ItemInOrder `json:"items"`
+	User  string        `json:"user"`
+}
+
+type ItemInOrder struct {
+	Name   string `json:"name"`
+	Amount int    `json:"amount"`
+}
+
+type OrderMsg struct {
+	Items []ItemInOrder `json:"items"`
+	User  string        `json:"user"`
+	Id    int64         `json:"id"`
+}
